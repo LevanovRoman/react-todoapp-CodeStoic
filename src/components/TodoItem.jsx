41e54a9301a5f57019d37799f038d05ffddb8b13
Леванов Р.Г. <r.levanov@nordsy.spb.ru>
2025-02-14 16:02:11 +0300

    first commit

---
+++ src/components/TodoItem.jsx
@@ -0,0 +1,22 @@
+import styles from "./todoitem.module.css";
+
+export default function TodoItem({item, todos, setTodos}){
+    function handleDelete(item) {
+        setTodos(todos.filter((todo) => todo !== item));
+    }
+
+    function handleClick() {
+
+    }
+
+    return <div className={styles.item}>
+        <div className={styles.itemName}>
+            <span onClick={() => handleClick(item.name)}>{item.name}</span>
+
+        <span>
+            <button onClick={() => handleDelete(item)} className={styles.deleteButton}>x</button>
+        </span>
+        </div>
+        <hr className={styles.line}/>
+        </div>;
+}
